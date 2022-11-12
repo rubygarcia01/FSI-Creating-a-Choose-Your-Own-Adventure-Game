ccac683d0d5ebe4d606c6e18c2cb1c80c071df0c
unknown <chortythemcflurry@gmail.com>
2022-11-11 21:24:58 -0500

    game

--- index.js
+++ index.js
@@ -11,22 +11,28 @@ if(firstAnswer === 'left'){
      }
      else if(secondAnswer === 'ignore'){
         let thirdAnswer = window.prompt('You decide to walk around the figure and ignore it. The short hallway then turns long and the ceiling lights become red. You turn around and see the figure start chasing you. You start running and running. After a minute the hallway seems endless, seeming to never stop. You finally come across two doors, one green and one blue. Which one will you open?')
+        
     }
+
 
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You decide to head right. As 
     you're walking down a hallway, you approach an office and see 
     a note on top of a desk. You open the note and a key falls out. 
     The note reads Level 0, Find The Exit.You turn around and two red 
-    doors stand in front of you. Which door will you open using the key?`)
+    doors stand in front of you, unattached to walls. Which door will you open using the key?`)
 
     if(secondAnswer === 'left door'){
         let thirdAnswer = window.alert('You decide to open the left door and go through it. You start falling endlessly, with no chance of it ever stopping. You fall indefinitely until the end of days. BOTTOMLESS ENDING!')
     }
     else if(secondAnswer === 'right door'){
         let thirdAnswer = window.prompt('You choose the right door and go through it. You feel dizzy and lightheaded...so you faint. You wake up in a strange parking lot, realizing youre in Level 1. Will you explore the place or stay where you are?')
-    }
+    } if(thirdAnswer === 'explore'){
+        let fourthAnswer = window.prompt('You chose to explore the parking lot. After a while, you come across a person, who seems to be relieved for finding you. Do you trust this person or leave it alone and keep exploring?')
+    } 
     
+
+
 
 
 }
