606ff5ec14850c8f54be5e167d90642906e82587
unknown <chortythemcflurry@gmail.com>
2022-11-11 19:28:36 -0500

    game

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let firstAnswer  = window.prompt('Do you head left or right?')
+let firstAnswer  = window.prompt('Suddenly, you wake up and notice you have noclipped into the backrooms successfully. Straight ahead there are two paths that lead left and right, which do you take?')
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
